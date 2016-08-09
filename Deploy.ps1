27706f76d462cd19cebc4f156601f20a8f94d929
Rupert Benbrook <rupert@benbrook.org.uk>
2016-08-09 21:15:35 +0100

    Switched to single resource group and template for simplicity.

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,64 +1,14 @@
 ﻿$timestamp = [int]((Get-Date).Subtract([datetime]"2016-08-08 00:00:00").TotalMinutes / 2)
 $rgPrefix = "rb" + $timestamp   
 $rgPrefix
-$coreRg = $rgPrefix + "core"
-$liveRg = $rgPrefix + "live"
-$nliveRg = $rgPrefix + "nlive"
-$path = [string](Resolve-Path ".")
-Save-AzureRmProfile -Path "$path\AzureProfile.json" -Force
-$coreJob = Start-Job -Name "Core" -ArgumentList ($path, $coreRg) -ScriptBlock {
-    param (
-        $path,
-        $rgPrefix
-    )
-    Select-AzureRmProfile -Path "$path\AzureProfile.json"
-    Select-AzureRmSubscription -SubscriptionName "Microsoft"
-    New-AzureRmResourceGroup -Name $rgPrefix -Location northeurope
-    $parameters = @{
-        "namePrefix" = $rgPrefix;
-        "locations" = @("northeurope", "westeurope");
-        "locationVnetPrefixes" = @("10.0.", "10.1.");
-        "adminUsername" = "Rupert";
-        "adminPassword" = "P@55W0rd123!"
-    }
-    $parameters
-    New-AzureRmResourceGroupDeployment -DeploymentName $rgPrefix -ResourceGroupName $rgPrefix -TemplateFile "$path\CoreNetwork.json" -TemplateParameterObject $parameters
+Select-AzureRmSubscription -SubscriptionName "Microsoft"
+New-AzureRmResourceGroup -Name $rgPrefix -Location "northeurope"
+New-AzureRmResourceGroupDeployment -DeploymentName $rgPrefix -ResourceGroupName $rgPrefix -TemplateFile ".\EnterpriseNetwork.json" -TemplateParameterObject @{
+    "namePrefix" = $rgPrefix;
+    "locations" = @("northeurope", "westeurope");
+    "coreLocationVnetPrefixes" = @("10.0.", "10.1.");
+    "liveLocationVnetPrefixes" = @("10.2.", "10.3.");
+    "nonLiveLocationVnetPrefixes" = @("10.4.", "10.5.");
+    "adminUsername" = "Rupert";
+    "adminPassword" = "P@55W0rd123!"
 }
-$liveJob = Start-Job -Name "Live" -ArgumentList ($path, $liveRg) -ScriptBlock {
-    param (
-        $path,
-        $rgPrefix
-    )
-    Select-AzureRmProfile -Path "$path\AzureProfile.json"
-    Select-AzureRmSubscription -SubscriptionName "Microsoft"
-    New-AzureRmResourceGroup -Name $rgPrefix -Location northeurope
-    $parameters = @{
-        "namePrefix" = $rgPrefix;
-        "locations" = @("northeurope", "westeurope");
-        "locationVnetPrefixes" = @("10.2.", "10.3.");
-        "adminUsername" = "Rupert";
-        "adminPassword" = "P@55W0rd123!"
-    }
-    $parameters
-    New-AzureRmResourceGroupDeployment -DeploymentName $rgPrefix -ResourceGroupName $rgPrefix -TemplateFile "$path\DeployNetwork.json" -TemplateParameterObject $parameters
-}
-$nliveJob = Start-Job -Name "Non-Live" -ArgumentList ($path, $nliveRg) -ScriptBlock {
-    param (
-        $path,
-        $rgPrefix
-    )
-    Select-AzureRmProfile -Path "$path\AzureProfile.json"
-    Select-AzureRmSubscription -SubscriptionName "Rupert Benbrook (MSDN)"
-    New-AzureRmResourceGroup -Name $rgPrefix -Location northeurope
-    $parameters = @{
-        "namePrefix" = $rgPrefix;
-        "locations" = @("northeurope", "westeurope");
-        "locationVnetPrefixes" = @("10.4.", "10.5.");
-        "adminUsername" = "Rupert";
-        "adminPassword" = "P@55W0rd123!"
-    }
-    $parameters
-    New-AzureRmResourceGroupDeployment -DeploymentName $rgPrefix -ResourceGroupName $rgPrefix -TemplateFile "$path\DeployNetwork.json" -TemplateParameterObject $parameters
-}
-Wait-Job -Job @($coreJob, $liveJob, $nliveJob)
-#Add-AzureRmVirtualNetworkPeering -Name "$rgPrefix-" -VirtualNetwork xxx -RemoteVirtualNetworkId xxx
