6c9f0be6f34ce59c70bbebcb8a25ad04a6a8236a
Rupert Benbrook <rupertb@microsoft.com>
2016-08-13 18:41:16 +0100

    Starting to limit things down using nsgs and tightening peering

--- Scripts/NvaConfig.ps1
+++ Scripts/NvaConfig.ps1
@@ -1,5 +1,12 @@
 # Install routing role on Windows
 Install-WindowsFeature -Name Routing -IncludeManagementTools
 
+# Download and install Microsoft Message Analyzer
+# TODO: Detect install and skip
+#$mmaInstall = "$env:TEMP\MessageAnalyzer.msi"
+#Invoke-WebRequest -Uri "https://download.microsoft.com/download/2/8/3/283DE38A-5164-49DB-9883-9D1CC432174D/MessageAnalyzer64.msi" -OutFile $mmaInstall
+#Start-Process -FilePath $mmaInstall -ArgumentList "/qn","/l*","$mmaInstall.log" -Wait
+#Remove-Item -Path $mmaInstall -Force
+
 # Run the VM config script
 & .\VmConfig.ps1
